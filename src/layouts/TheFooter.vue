c11f5ef4ebc4d817c1938a3ce6f01de4f3c5cbc0
peterkitonga <pkitonga.pk@gmail.com>
2021-12-28 22:52:38 +0300

    Add boilerplate markup for the layouts

---
+++ src/layouts/TheFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer></footer>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class TheFooter extends Vue {}
+</script>
---
+++ src/layouts/ThePageHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <article class="page-header"></article>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class ThePageHeader extends Vue {}
+</script>
