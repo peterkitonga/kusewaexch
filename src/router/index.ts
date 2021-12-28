319883b537b7f4dfdb4483b0ef47fd538f6a3015
peterkitonga <pkitonga.pk@gmail.com>
2021-12-28 22:53:49 +0300

    Add boilerplate markup for the views and their respective routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,50 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
+
+// Layouts
+import AppHeader from "../layouts/TheHeader.vue";
+import AppPageHeader from "../layouts/ThePageHeader.vue";
+import AppFooter from "../layouts/TheFooter.vue";
+
+// App Views
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Dashboard from "../views/Dashboard.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: () => import("../views/About.vue"),
+    component: () => import("../layouts/TheWrapper.vue"),
+    children: [
+      {
+        path: "/",
+        name: "app.home",
+        components: {
+          header: AppHeader,
+          default: Home,
+          footer: AppFooter,
+        },
+      },
+      {
+        path: "/login",
+        name: "app.login",
+        components: {
+          default: Login,
+        },
+      },
+      {
+        path: "/home",
+        name: "app.dashboard",
+        components: {
+          header: AppHeader,
+          "page-header": AppPageHeader,
+          default: Dashboard,
+          footer: AppFooter,
+        },
+      },
+    ],
   },
 ];
 
@@ -21,6 +52,18 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.hash) {
+      return {
+        selector: to.hash,
+        behavior: "smooth",
+      };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
 });
 
 export default router;
