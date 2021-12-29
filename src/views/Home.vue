80019d46604dbf567060db4d5c67fe9bb90a2ced
peterkitonga <pkitonga.pk@gmail.com>
2021-12-29 08:45:28 +0300

    Add markup & styling for the hero section in the home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,28 @@
 <template>
-  <main></main>
+  <main>
+    <section class="hme-hero">
+      <div class="container">
+        <div class="hme-hero__card">
+          <h1 class="hme-hero__card-title heading-one">Secret Santa</h1>
+          <p class="hme-hero__card-text">
+            A free and easy to use gift exchange for any loving family. Organize
+            your secret santa in easy steps and draw names set up by the group
+            santa.
+          </p>
+          <router-link to="/#get-started" class="btn btn--primary"
+            >Start your secret santa&nbsp;<i
+              class="bi bi-chevron-double-right"
+            ></i
+          ></router-link>
+        </div>
+        <img
+          class="hme-hero__img"
+          src="../assets/images/jpg/pexels-karolina-grabowska-5469811.jpg"
+          alt="Christmas Tree"
+        />
+      </div>
+    </section>
+  </main>
 </template>
 
 <script lang="ts">
