0b5492ad1ababec92c715948882c8fbe10a1f5a9
peterkitonga <pkitonga.pk@gmail.com>
2021-12-29 18:24:44 +0300

    Add markup and styling for the footer

--- src/layouts/TheFooter.vue
+++ src/layouts/TheFooter.vue
@@ -1,10 +1,21 @@
 <template>
-  <footer></footer>
+  <base-footer>
+    <template v-slot:main>
+      <h4 class="heading-four footer__content-heading">
+        Copyright © 2021 KXCH. All Rights Reserved
+      </h4>
+    </template>
+  </base-footer>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import BaseFooter from "@/components/BaseFooter.vue";
 
-@Component
+@Component({
+  components: {
+    BaseFooter,
+  },
+})
 export default class TheFooter extends Vue {}
 </script>
