275fa6cb7e5cdac0fb0c06793cc3481b205f4c9b
peterkitonga <pkitonga.pk@gmail.com>
2021-12-29 20:40:36 +0300

    Add configuration for sourcemaps

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  css: {
+    sourceMap: process.env.NODE_ENV !== "production",
+  },
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = process.env.VUE_APP_NAME || "KXCH";
