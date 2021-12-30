61f974d6a158e7d804f148b71fac05fd32374e81
peterkitonga <pkitonga.pk@gmail.com>
2021-12-30 19:05:51 +0300

    Modify the base input component

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -7,14 +7,18 @@
     :name="name"
   >
     <input
+      :id="`${name}Input`"
       :type="type"
       :name="name"
       class="form__field-control"
       :class="classes"
       :placeholder="placeholder"
-      @input="onInput"
-      :value="value"
+      :value="inputValue"
+      v-model="model"
     />
+    <label v-if="label" :for="`${name}Input`" class="form__field-label">{{
+      label
+    }}</label>
     <small v-if="errors[0]" class="form__field-help text-primary">{{
       errors[0]
     }}</small>
@@ -32,11 +36,21 @@ export default class BaseInput extends Vue {
   @Prop({ required: true, type: String }) readonly type!: string;
   @Prop({ required: true, type: Object as PropType<InputValidationRule> })
   readonly rules!: string;
-  @Prop({ required: true, type: String }) readonly placeholder!: string;
-  @Prop({ required: true, type: String }) readonly value!: string;
+  @Prop({ required: false, type: String }) readonly placeholder!: string;
+  @Prop({ required: true }) readonly inputValue!: unknown;
+  @Prop({ required: false, type: String }) readonly label!: string;
+
+  get model(): unknown {
+    return this.inputValue;
+  }
+
+  set model(newValue: unknown) {
+    this.$emit("input", newValue);
+  }
 
   public onInput(event: Event): void {
     const targetEl = event.target as HTMLInputElement;
+
     this.$emit("input", targetEl.value);
   }
 }
